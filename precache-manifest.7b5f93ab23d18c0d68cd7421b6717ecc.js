ac10706137db481d93ec7b1a18d8a26b2e20dc05
Joana Leitão Oliveira <joanaleitaooliveira@MacBook-Air-de-Joana.local>
2020-06-19 17:18:21 +0100

    Updates

---
+++ precache-manifest.7b5f93ab23d18c0d68cd7421b6717ecc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0933081e103f656bb9ea1a52f3f09dc",
+    "url": "/milestone2/index.html"
+  },
+  {
+    "revision": "945ef7f98796ef8f7b69",
+    "url": "/milestone2/static/css/main.d1486dd7.chunk.css"
+  },
+  {
+    "revision": "c194df6042a33740d15b",
+    "url": "/milestone2/static/js/2.9806993d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/milestone2/static/js/2.9806993d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945ef7f98796ef8f7b69",
+    "url": "/milestone2/static/js/main.d346f6b8.chunk.js"
+  },
+  {
+    "revision": "69f2675479a5ff520ea1",
+    "url": "/milestone2/static/js/runtime-main.ea631211.js"
+  }
+]);
